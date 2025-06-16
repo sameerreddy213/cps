f492ed6cef9473453b5a8cdb21187d801b74e904
Phoenix-0001 <149677560+Phoenix-0001@users.noreply.github.com>
2025-06-16 19:45:16 +0530

    Fix commit issues

--- team-parthan-assessment/backend/src/middlewares/uploadMiddleware.ts
+++ team-parthan-assessment/backend/src/middlewares/uploadMiddleware.ts
@@ -1,11 +1,6 @@
 import multer, { diskStorage } from 'multer';
 import path from 'path';
 
-<<<<<<< HEAD:team-parthan-assessment/backend/src/middlewares/uploadMiddleware.ts
-
-
-const upload = multer({ dest: 'uploads/' });
-=======
 const storage = diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) =>
@@ -13,5 +8,4 @@ const storage = diskStorage({
 });
 
 const upload = multer({ storage });
->>>>>>> aba7c5d6916189a4a4cfee98ddb1f6e0ed96a84d:project-assessment/backend/middlewares/uploadMiddleware.ts
 export default upload;
--- team-parthan-assessment/backend/src/server.ts
+++ team-parthan-assessment/backend/src/server.ts
@@ -1,22 +1,13 @@
 import express, { json, Request, Response, NextFunction } from "express";
 import cors from "cors";
-<<<<<<< HEAD:team-parthan-assessment/backend/src/server.ts
-import authRoutes from "./routes/authRoute";
-import analyzeRoute from "./routes/analyzeRoute";
-=======
 import apiRoutes from "./routes/api";
 import authRoutes from "./routes/authRoute";
->>>>>>> aba7c5d6916189a4a4cfee98ddb1f6e0ed96a84d:project-assessment/backend/server.ts
 
 const app = express();
 
 app.use(cors());
 app.use(json());
-<<<<<<< HEAD:team-parthan-assessment/backend/src/server.ts
-app.use("/api", analyzeRoute);
-=======
 app.use("/api", apiRoutes);
->>>>>>> aba7c5d6916189a4a4cfee98ddb1f6e0ed96a84d:project-assessment/backend/server.ts
 app.use("/api/auth", authRoutes);
 
 // Error handling middleware
