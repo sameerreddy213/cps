80d920d3bcacca6e0161fc7bc04b8c178228ff5f
Subathra Ravi <subathraravi@Subathras-MacBook-Air.local>
2025-06-24 21:37:18 +0530

    Code:  Adding AuthContext.tsx, Home.tsx, Navbar.tsx ,Login.tsx

--- our-app/client/src/main.tsx
+++ our-app/client/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from './components/AuthContext'; // adjust path as needed
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <Router>
+    <AuthProvider>
     <App />
+    </AuthProvider>
+  </Router>
   </StrictMode>,
 )
