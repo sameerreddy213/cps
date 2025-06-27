0dff54a2bf63f2898144d6fefd24e9e329e7a6d8
Nakshatra-B <120786002+Nakshatra-B@users.noreply.github.com>
2025-06-27 10:29:40 +0530

    Modified frontend code to call from eduassess render backend instead of localhost

--- frontend/src/components/changepassword.tsx
+++ frontend/src/components/changepassword.tsx
@@ -22,7 +22,7 @@ const ChangePassword: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     }
 
     try {
-        const res = await fetch('http://localhost:5000/api/auth/change-password', {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/change-password`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
